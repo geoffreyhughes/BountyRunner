6718b7997d44f4c53d15f9fe30e3aee0d4e50861
Geoffrey Hughes <46589744+geoffreyhughes@users.noreply.github.com>
2020-11-09 19:12:25 -0800

    Initial Commit

---
+++ BountyRunner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BountyRunner
+//
+//  Created by Geoffrey Hughes on 11/9/20.
+//  Copyright © 2020 Geoffrey Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
